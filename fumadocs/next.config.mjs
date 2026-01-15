83abb1649d8978268ed7a836d7c5fd98ee85634a
Rafal Szczepanik <rafal.sz@tutanota.com>
2026-01-15 22:19:08 +0100

    fix: update config

--- fumadocs/next.config.mjs
+++ fumadocs/next.config.mjs
@@ -1,11 +1,16 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-  output: 'export',
+  output: "export",
   reactStrictMode: true,
+  basePath: "/react-synth",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withMDX(config);
